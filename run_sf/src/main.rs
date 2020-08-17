e06ccb8988708e66b4e5a67832ea991223fb6325
Hanus Krett Hansen <hkh@82.211.205.205>
2020-08-17 17:02:04 +0200

    Uploading for sharing with labtop

--- run_sf/src/main.rs
+++ run_sf/src/main.rs
@@ -1,5 +1,15 @@
 use seq_flush::*;
+use std::net::UdpSocket;
 
 fn main() {
     println!("Hello, world!");
+    let socket_server = seq_flush::server::init_bind("77777");
+
+
+    let socket_client = seq_flush::client::init_rec(8000, 8009);
+
+    seq_flush::server::make_address([], "127.0.0.1::");
+
+    
+
 }
--- seq_flush/src/lib.rs
+++ seq_flush/src/lib.rs
@@ -8,7 +8,7 @@ use std::thread;
 pub mod server{
     use super::*;
 
-    pub fn init_bind(port:String)->UdpSocket{
+    pub fn init_bind(port:&str)->UdpSocket{
         
         let socket = UdpSocket::bind(format!("{}{}","127.0.0.1::", port)).unwrap();
         return socket;
@@ -16,18 +16,26 @@ pub mod server{
 
     
     // Splits the file into several file handlers
-    pub fn init_file(path_file:String, num_seq:i32)->Vec<File>{
+    pub fn init_file(path_file:String, num_seq:i32, seq_number:f32)->(Vec<File>,u64,u64){
         
         let file = File::open(&path_file).unwrap();
         let metadata = file.metadata().unwrap();
         let mut filehandles:Vec<File> = Vec::new();
-        //let size = metadata.len()/num_seq;
-        for n in 0..=9 {
+        
+        let size = metadata.len();
+        let seq = (size as f32) / seq_number;
+        let temp = seq % 1 as f32;
+        let seq = ((1 as f32 - temp) + seq) as u64;
+        let delta = (seq_number as u64 * seq) - size;
+        let seq_last = seq - delta;
+
+        for n in 0..=seq_number as u64 {
             let mut file = File::open(&path_file).unwrap();
-            file.seek(SeekFrom::Start(n*100)).unwrap();
+            file.seek(SeekFrom::Start(n*seq)).unwrap();
             filehandles.push(file)
         }
-        filehandles
+        //returns the file handlers and the size of each seq and of the last one
+        (filehandles,seq,seq_last)
         
     }
 
@@ -38,6 +46,14 @@ pub mod server{
             sockets.push(UdpSocket::bind("0.0.0.0:0").unwrap());
         }
         sockets
+    }
+
+    pub fn make_address(range:&[u64], addr: &str)->Vec<String>{
+        let mut address:Vec<String> = Vec::new();
+        for port in range.iter(){
+            address.push(format!("{}{}",addr, port));
+        }
+        address
     }
 
     pub fn connect(addres:Vec<String>, mut sockets:Vec<UdpSocket>)->Vec<UdpSocket>{
@@ -61,8 +77,17 @@ pub mod server{
 }
 
 pub mod client {
+    use super::*;
 
-    pub fn init_rec(){}
+    pub fn init_rec(start:u64, end:u64)->Vec<UdpSocket>{
+        let mut address:Vec<UdpSocket> = Vec::new();
+        for port in start..=end{
+            address.push(UdpSocket::bind(format!("{}{}","127.0.0.1::", port)).unwrap());
+        }
+        address
+    }
+
+
 
 
 }
